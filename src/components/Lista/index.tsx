a74883a2bae1c5986fed07a21657646181358a75
Danilo Scipioni <danilo.scipioni@schott.com>
2022-03-18 14:29:10 -0300

    Adicionado Tarefa

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,30 +1,19 @@
-import React from 'react';
+import React  from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Item from './item';
 import style from './Lista.module.scss';
 
-function Lista() {
-    const tarefas = [
-        {
-            tarefa: 'React',
-            tempo: '02:00:00'
-        },
-        {
-            tarefa: 'Javascript',
-            tempo: '01:00:00'
-        },
-        {
-            tarefa: 'Typescript',
-            tempo: '01:00:00'
-        }
-    ];
+
+function Lista( { tarefas }: { tarefas: ITarefa[]}) {
+    
 
     return (
         <aside className={style.listaTarefas}>
             <h2> Estudos dos dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <Item 
-                    key={index}
+                    <Item
+                        key={index}
                         {...item}
                     />
                 ))}
